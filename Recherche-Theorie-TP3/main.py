1239ae06ba6a81ce9d6dc27f28ef5268141b922b
Matteo466466 <117832217+Matteo466466@users.noreply.github.com>
2025-12-12 13:29:26 +0100

    Delete Recherche-Theorie-TP3 directory

--- Recherche-Theorie-TP3/main.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-from transformers import AutoModelForSeq2SeqLM
-from transformers import AutoTokenizer
-from transformers import GenerationConfig
-
-model_name='google/flan-t5-base'
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
